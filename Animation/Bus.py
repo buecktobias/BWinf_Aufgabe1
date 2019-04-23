94f8d1f13cffcb0a26fc8aabb0ea80053e52bbf9
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-04-23 10:23:01 +0200

    Animation changed image

--- Animation/Bus.py
+++ Animation/Bus.py
@@ -1,21 +1,30 @@
 import tkinter
-
+from PIL import Image, ImageTk
+from PIL import *
 
 class Bus:
     def __init__(self, canvas: tkinter.Canvas, x, y, width, height, x_speed, y_speed):
         self.canvas = canvas
-        self.image = canvas.create_oval(x, y, x+width, y+height, outline="yellow", fill="yellow")
         self.x = x
         self.y = y
         self.x_speed = x_speed
         self.y_speed = y_speed
         self.width = width
         self.height = height
+        self.image = None
+        self.show()
 
     def update(self):
         self.x += self.x_speed
         self.y += self.y_speed
         self.canvas.delete(self.image)
-        self.image = self.canvas.create_oval(round(self.x), round(self.y), round(self.x) + self.width, round(self.y) + self.height, outline="yellow", fill="yellow")
+        self.show()
+
+    def show(self):
+        pil_image: Image = Image.open("Bus.jpg")
+        pil_image = pil_image.resize((20, 20))
+        img = ImageTk.PhotoImage(pil_image)
+        self.image = self.canvas.create_image(self.x, self.y, image=img, anchor="nw")
         self.canvas.pack()
         self.canvas.update()
+
