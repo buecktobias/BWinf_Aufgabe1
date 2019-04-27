00ab4117a95d868b1d137c2fca22507b1ff99adc
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-04-27 13:56:37 +0200

    Polygons have names and names get printet

--- Animation/Polygon.py
+++ Animation/Polygon.py
@@ -5,6 +5,7 @@ from shapely.geometry.polygon import Polygon as ShapelyPolygon
 class Polygon:
     def __init__(self, edges: list, name: str):
         self.name = name
+        print(self.name)
         self._edges = edges
         self.shapely_polygon: ShapelyPolygon = ShapelyPolygon(edges)
 
